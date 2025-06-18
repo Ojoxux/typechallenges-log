99eeea00c80cf36c604bb4b7ac86a7460cf83fd6
Jou Okuyama <ojoxux@gmail.com>
2025-06-18 14:11:08 +0900

    fix: pick関数のインデントをprettierで修正

--- chatgpt/pick/hard/index2.ts
+++ chatgpt/pick/hard/index2.ts
@@ -7,9 +7,9 @@ type MyPick2<T, K extends keyof T> = {
 };
 
 function pick<T, K extends keyof T>(obj: T, keys: K[]): MyPick2<T, K> {
-    const result = {} as MyPick2<T,K>
-    for (const key of keys) {
-        result[key] = obj[key];
-    }
-    return result;
+  const result = {} as MyPick2<T, K>;
+  for (const key of keys) {
+    result[key] = obj[key];
+  }
+  return result;
 }
