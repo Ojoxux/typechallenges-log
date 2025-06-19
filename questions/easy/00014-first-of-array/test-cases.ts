92a6baa8d22080b6809864e3973df07f92c4351e
Jou Okuyama <ojoxux@gmail.com>
2025-06-19 13:59:02 +0900

    solve: 14-First of Arrayを解いた

---
+++ questions/easy/00014-first-of-array/test-cases.ts
@@ -0,0 +1,14 @@
+import { Equal, Expect } from '@type-challenges/utils'
+import type { First } from './index'
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
+
+type errors = [
+  First<'notArray'>,
+  First<{ 0: 'arrayLike' }>,
+]
